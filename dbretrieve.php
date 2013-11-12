7e48d7cf64b1723c0d3ca5d7b82bd6d57373f431
Benjamin Kaufman <blkaufma@uvm.edu>
2013-11-12 10:41:44 -0500

    Create dbretrieve.php
    function works

---
+++ dbretrieve.php
@@ -0,0 +1,10 @@
+<?php
+	require("dbconnect.php");
+	$pagetop = $_POST['page'] * 15;
+	$pagebottom = $pagetop-15;
+	$query = "SELECT * FROM Cards ORDER BY name Asc LIMIT $pagebottom, $pagetop";
+	$getResults = $db->prepare($query);
+	$getResults->execute();
+	$results = $getResults->fetchAll();
+	echo json_encode($results);
+?>
