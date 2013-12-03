d676f1bdac949740d05ddd77ad42b5128ee084a3
Benjamin Kaufman <blkaufma@uvm.edu>
2013-12-02 23:18:59 -0500

    Update home.php

--- home.php
+++ home.php
@@ -1,5 +1,7 @@
 <?php include "top.php" ?>
-
+<div><h2 class="center-block" > Join Today to Create your Online Collection<small> It's Free!</small></h2>
+				<a class="btn btn-primary navbar-btn icon-ok-circle" data-toggle="modal" data-target="#signupmodal" id="signup-btn">   Sign Up!</a>    
+</div>
 		<!--Body-->
         <div class="container">  
 			<!--Carousel Container-->
@@ -7,37 +9,38 @@
 				<!--Carousel-->
 				<div id="trade-carousel" class="carousel slide" data-interval="false"><!-- class of slide for animation -->
 					<div class="carousel-inner">
-						<div class="item active text-center"><!-- class of active since it's the first item -->
-							<img src="Images/image1.jpg" alt="" />
-							<div class="carousel-caption">
-								<h3>Want to trade for this card?</h3>
-								<p>Or search cards we have for trade</p>
-								<input type="text" placeholder="Enter Search Term Here" style="width:auto;" class="carousel-input">
-								<button type="submit" class="btn btn-success">Search</button>
-							</div>
-						</div>
-						<div class="item text-center">
+						<div class="item active text-center">
 							<img src="Images/image1.jpg" alt="" />
 								<div class="carousel-caption">
 									<h3>Want to trade for this card?</h3>
-									<p>Or search cards we have for trade</p>
-									<input type="text" placeholder="Enter Search Term Here" style="width:auto;" class="carousel-input">
-									<button type="submit" class="btn btn-success">Search</button>
+							<p>Or search cards we have for trade</p>
+								<input type="text" placeholder="Enter Search Term Here" style="width:auto;" class="carousel-input">
+								<button type="submit" class="btn btn-success">Search</button>
 								</div>
 						</div>
 						<div class="item text-center">
-							<img src="Images/image1.jpg" alt="" />
+							<img src="Images/image2.jpg" alt="" />
+								<div class="carousel-caption">
+									<h3>Want to trade for this card?</h3>
+							<p>Or search cards we have for trade</p>
+								<input type="text" placeholder="Enter Search Term Here" style="width:auto;" class="carousel-input">
+								<button type="submit" class="btn btn-success">Search</button>
+								</div>
+						</div>
+						<div class="item text-center">
+							<img src="Images/image3.jpg" alt="" />
 							<div class="carousel-caption">
 								<h3>Want to trade for this card?</h3>
-								<p>Or search cards we have for trade</p>
+							<p>Or search cards we have for trade</p>
 								<input type="text" placeholder="Enter Search Term Here" style="width:auto;" class="carousel-input">
 								<button type="submit" class="btn btn-success">Search</button>
 							</div>
 						</div>
 						<div class="item text-center">
-							<img src="Images/image1.jpg" alt="" />
+							<img src="Images/image4.jpg" alt="" />
 							<div class="carousel-caption">
 								<h3>Want to trade for this card?</h3>
+						
 								<p>Or search cards we have for trade</p>
 								<input type="text" placeholder="Enter Search Term Here" style="width:auto;" class="carousel-input">
 								<button type="submit" class="btn btn-success">Search</button>
@@ -45,12 +48,10 @@
 						</div>
 					</div><!-- /.carousel-inner -->
 					<!--  Next and Previous controls below href values must reference the id for this carousel -->
-					<a class="carousel-control left" href="#trade-carousel" data-slide="prev"></a>
-					<a class="carousel-control right" href="#trade-carousel" data-slide="next"></a>
+					<a class="carousel-control left" href="#trade-carousel" data-slide="prev"><i class="icon-arrow-left"></i></a>
+					<a class="carousel-control right" href="#trade-carousel" data-slide="next"><i class="icon-arrow-right"></i></a>
 				</div><!-- /.carousel -->   
 			</div>        
 		</div><!-- End Body -->
-<?php include "signup.php" ?>
-<?php include "loginmodal.php" ?>
 	</body>
 </html>
