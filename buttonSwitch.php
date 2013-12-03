8209e136fadc4e15cc204ac6d612e0fcec593978
Benjamin Kaufman <blkaufma@uvm.edu>
2013-12-02 23:12:29 -0500

    Update buttonSwitch.php

--- buttonSwitch.php
+++ buttonSwitch.php
@@ -1,10 +1,10 @@
 <?php
 	if(isset($_SESSION['user'])){
 		$array = $_SESSION['user'];
-		echo '<p id="loggedIn" class="navbar-text">Logged in as: '.$array['name'].'</p>';
-		echo '<a class="btn btn-primary navbar-btn btn-sm pull-right" data-toggle="modal" data-target="#loginmodal" id="logout-btn">Sign Out</a>';
+		echo '<p id="loggedIn" class="navbar-text navbar-inverse text-info">Logged in as: '.$array['name'].'</p>';
+		echo '<a class="btn btn-primary pull-right icon-user navbar-btn" data-toggle="modal" id="logout-btn" onclick="/~blkaufma/cs148/final/logout.php"> Sign Out</a>';
 	}//end if
 	else{
-		echo '<a class="btn btn-primary navbar-btn pull-right" data-toggle="modal" href="#loginmodal" id="log-btn">Sign In</a>';
+		echo '<a class="btn btn-primary pull-right icon-user navbar-btn" data-toggle="modal" href="#loginmodal" id="log-btn"> Sign In</a>';
 	}//end else
 ?>
