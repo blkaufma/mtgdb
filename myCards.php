ab1a1dc1c703a99fcdc7aec7c556b48b1726140c
Benjamin Kaufman <blkaufma@uvm.edu>
2013-12-02 23:20:30 -0500

    Update myCards.php

--- myCards.php
+++ myCards.php
@@ -8,13 +8,16 @@
 		<!--Body-->
         <div class="row" style="width:100%">
         	<div class="container">      
-				<a class="btn btn-primary navbar-btn btn-sm" data-toggle="modal" data-target="#addcardform" id="addcard-btn">Add a card!</a>    
+				<a class="btn btn-primary navbar-btn btn-sm icon-pencil admin" data-toggle="modal" data-target="#addcardform" id="addcard-btn">Add a card!</a>    
 				<table class="table table-striped table-hover table-bordered" id="dblist-table">
                 <thead>
 					<tr>
-						<th>Card Name</th>
-						<th>Card Type</th>
-						<th>Number available</th>
+						<th>Remove</th>
+						<th>Name</th>
+						<th>Type</th>
+						<th>Color</th>
+						<th>Rarity</th>
+						<th>Count</th>
 					</tr>
                 </thead>
                 	<tbody>
@@ -74,6 +77,49 @@
                 </div>
             </div>
         </div>
+		<div class="modal fade" id="ViewCardModal"><!-- ViewCardModal -->
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true" id="ViewCardModal-close">&times;</button>
+						<h4 class="modal-title" id="cardName">Name</h4>
+                        <div id="warnings" class="alert" style="margin-bottom:0;"></div>
+					</div>
+					<div class="modal-body span12" id="cardViewForm">
+						<div class="cardFrame modal-content">
+						<img class="cardPicture" width="250" height="300"> 
+						</div>
+							
+						<div id="allTypes" class="likeSection modal-content"> 
+							<div id="Type">Type:</div>
+							<div id="Supertype">Supertype:</div>
+							<div id="Subtype">Subtype:</div>
+						</div>
+						<div class="likeSection modal-content">
+							<div id="Color">Color:</div>
+							<div id="theCost">Cost:</div>
+							<div id="Rarity">Rarity:</div>
+						</div>
+						<div class="likeSection modal-content">
+							<div id="Ability">Ability:</div>
+						</div>
+						<div id="Stock" class="likeSection modal-content">
+							<div id="Count">Count:</div>
+							<div id="Available">Available:</div>
+							<div id="Used">Used:</div>
+						</div>
+						<div class="likeSection modal-content">
+							<div id="Power">Power:</div>
+							<div id="Toughness">Toughness:</div>
+						</div>
+						<div class="btn-group" id="end-place">
+  								<button class="btn" id="edit"><i class="icon-wrench"></i>Edit</button>
+                                <button class="btn" type="submit" id="edit-submit"><i class="icon-download-alt"></i> Submit</button>
+  							</div>
+					</div>
+				</div><!-- /.modal-content -->
+			</div><!-- /.modal-dialog -->
+		</div>
 	</body>
 	<script>
 	var page = 1;
@@ -87,14 +133,56 @@
 			for(var i=0;i<data.length;i++){
 				var holder = (i+1).toString();
 				holder = "#".concat(holder);
-				$(holder).append("<td>"+data[i]['CardID']+"</td>");
+				$(holder).append("<td><button class='btn deleteCard'><i class='icon-trash'></i>Delete</button></td>");					   
+				$(holder).append("<td>"+data[i]['name']+"</td>");
 				$(holder).append("<td>"+data[i]['type']+"</td>");
-				$(holder).append("<td>"+data[i]['available']+"</td>");
+				$(holder).append("<td>"+data[i]['color']+"</td>");
+				$(holder).append("<td>"+data[i]['rarity']+"</td>");      
+				$(holder).append("<td><button class='btn btn-success increaseMyCardCount'><i class='icon-arrow-up'></i></button><button class=''>" +data[i]['count']+"</button><button class='btn btn-danger decreaseMyCardCount'><i class='icon-arrow-down'></i></button></td>");      
 				$(holder).attr('data-id',data[i]['id']);
+				$(holder).on('click',cardLook);
 			}//end for
 		}//end success function
-	});//end happen
-	}
+	});//end ajax call
+	}//end happen
+	function unHappen(){
+		for(var i=1;i<=15;i++){
+			var holder = (i).toString();
+			holder = "#".concat(holder);
+			$(holder).html("");
+		}//end for
+	}//end unHappen
+	function cardLook(){
+		var id = $(this).attr('data-id');
+		//alert(id);
+		$.ajax({
+			type:'POST',
+			url:'/~blkaufma/cs148/final/MyCardRetrieve.php',
+			data:{'id':id},
+			dataType:'json',
+			success:function(data){
+				var cost = data[0]['cost'];
+				$("#cardName").html("Name: "+data[0]['name']);
+				$("#Type").html("Type: "+data[0]['type']);
+				$("#Supertype").html("Supertype: "+data[0]['supertype']);
+				$("#Subtype").html("Subtype: "+data[0]['subtype']);
+				$("#Color").html("Color: "+data[0]['color']);
+				$("#theCost").html("Cost: "+data[0]['cost']);
+				$("#Rarity").html("Rarity: "+data[0]['rarity']);
+				$("#Ability").html("Ability: "+data[0]['abilities']);
+				$("#Count").html("Count: "+data[0]['count']);
+				$("#Available").html("Available: "+data[0]['available']);
+				$("#Used").html("Used: "+data[0]['used']);
+				$("#Power").html("Power: "+data[0]['power']);
+				$("#Toughness").html("Toughness: "+data[0]['toughness']);
+				$("#edit").attr('data-id',id);
+				//$("#cardPicture").append(“ “+data['color']);
+				$("#ViewCardModal").modal('show');
+				
+			},//end success function
+			error:function(){alert("fail")}//end error statement
+		});//end ajax call
+	}//end cardLook
 		$(document).ready(function(e){
 			var pages = $.ajax({
 				type:'POST',
@@ -143,9 +231,86 @@
 					}//end else if
 					$("#pagination-links").append("<li class='pagination-link' id='forwardPage'>&rsaquo;</li>");
 					$("#pagination-links").append("<li class='pagination-link' id='lastPage'>&raquo;</li>");
+					$("#edit-submit").hide();
 					happen();
 				}//end success
 			});//end pages function
+			$("#edit").click(function(){
+				var cardName = $("#cardName").text().slice(6);
+				$("#cardName").html("Name: "+"<input class='EditField' id='cardNameEdit' type='text' name='cardName' value='"+cardName+"'>");
+				var Type = $("#Type").text().slice(6);
+				$("#Type").html("Type: "+"<input class='EditField' id='typeEdit' type='text' name='Type' value='"+Type+"'>");
+				var Supertype = $("#Supertype").text().slice(11);
+				$("#Supertype").html("Supertype: "+"<input class='EditField' id='supertypeEdit' type='text' name='Supertype' value='"+Supertype+"'>");
+				var Subtype = $("#Subtype").text().slice(9);
+				$("#Subtype").html("Subtype: "+"<input class='EditField' id='subtypeEdit' type='text' name='Subtype' value='"+Subtype+"'>");
+				var Color = $("#Color").text().slice(7);
+				$("#Color").html("Color: "+"<input class='EditField' id='colorEdit' type='text' name='Color' value='"+Color+"'>");
+				var theCost = $("#theCost").text().slice(6);
+				$("#theCost").html("Cost: "+"<input class='EditField' id='theCostEdit' type='text' name='theCost' value='"+theCost+"'>");
+				var Rarity = $("#Rarity").text().slice(8);
+				$("#Rarity").html("Rarity: "+"<input class='EditField' id='rarityEdit' type='text' name='Rarity' value='"+Rarity+"'>");
+				var Ability = $("#Ability").text().slice(9);
+				$("#Ability").html("Ability: "+"<textarea class='EditField2' id='abilityEdit' type='text' name='Ability' value='"+Ability+"'>");
+				$("#abilityEdit").val(Ability);
+				var Count = $("#Count").text().slice(7);
+				$("#Count").html("Count: "+"<input class='EditField' id='countEdit' type='text' name='Count' value='"+Count+"'>");
+				var Available = $("#Available").text().slice(11);
+				$("#Available").html("Available: "+"<input class='EditField' id='availableEdit' type='text' name='Available' value='"+Available+"'>");
+				var Used = $("#Used").text().slice(6);
+				$("#Used").html("Used: "+"<input class='EditField' type='text' id='usedEdit' name='Used' value='"+Used+"'>");
+				var Power = $("#Power").text().slice(7);
+				$("#Power").html("Power: "+"<input class='EditField' type='text' id='powerEdit' name='Power' value='"+Power+"'>");
+				var Toughness = $("#Toughness").text().slice(11);
+				$("#Toughness").html("Toughness: "+"<input class='EditField' id='toughnessEdit' type='text' name='Toughness' value='"+Toughness+"'>");
+				$("#edit").hide();
+				$("#edit-submit").show();
+			});//end edit
+			$(document).on('hide.bs.modal',function(){
+				$("#edit-submit").hide();
+				$("#edit").show();
+			});//switches the submit button back to the edit button
+			$("#edit-submit").click(function(){
+				var cardName = $("#cardNameEdit").val();
+				var cardType = $("#typeEdit").val();
+				var Supertype = $("#supertypeEdit").val();
+				var Subtype = $("#subtypeEdit").val();
+				var Color = $("#colorEdit").val();
+				var theCost = $("#theCostEdit").val();
+				var Rarity = $("#rarityEdit").val();
+				var Ability = $("#abilityEdit").val();
+				var Count = $("#countEdit").val();
+				var Available = $("#availableEdit").val();
+				var Used = $("#usedEdit").val();
+				var Power = $("#powerEdit").val();
+				var Toughness = $("#toughnessEdit").val();
+				var edit = $("#edit").attr('data-id');
+				$.ajax({
+					type:"POST",
+					url:'/~blkaufma/cs148/final/MyCardsUpdate.php',
+					data:{
+						'cardName':cardName,					
+						'type':cardType,	
+						'supertype':Supertype,
+						'subtype':Subtype,
+						'color':Color,
+						'cost':theCost,
+						'rarity':Rarity,
+						'ability':Ability,
+						'count':Count,
+						'available':Available,
+						'used':Used,
+						'power':Power,
+						'toughness':Toughness,
+						'id':edit
+					},//end data(finally)
+					success:{},
+					error:(function(xhr){alert("something went wrong "+ xhr.statusText)})
+				})//end ajax call
+				unHappen();
+				happen();
+			//	$("#ViewCardModal").modal('hide');
+			});//end submit
         });
 	</script>
 </html>
