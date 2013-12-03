a042250930ea933353bc6ed59101aac1c0bad70c
Benjamin Kaufman <blkaufma@uvm.edu>
2013-12-02 23:17:44 -0500

    Create dbretrieveMyDecks.php

---
+++ dbretrieveMyDecks.php
@@ -0,0 +1,16 @@
+<?php
+	require("dbconnect.php");
+	if(empty($_SESSION['user'])){
+		header("Location: home.php");
+		die();
+	}//end if
+	$array = $_SESSION['user'];
+	$username = $array['name'];
+	$pagetop = $_POST['page'] * 15;
+	$pagebottom = $pagetop-15;
+	$query = "SELECT * FROM Decks WHERE UserID='$username' ORDER BY name Asc LIMIT $pagebottom, $pagetop;";
+	$getResults = $db->prepare($query);
+	$getResults->execute();
+	$results = $getResults->fetchAll();
+	echo json_encode($results);
+?>
