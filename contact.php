21de94e888eebc4975b08c741ea3fdbc0b9f0de8
Benjamin Kaufman <blkaufma@uvm.edu>
2013-12-02 23:13:48 -0500

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,37 @@
+<?php include "top.php" ?>
+
+<div class="container text-center">
+<address> 
+  <strong>Magic Database Headquarters</strong><br>
+  88 Grant Street <br>
+  Burlington, VT 05405<br>
+  <abbr title="Phone">P:</abbr> (301) 648-3520
+</address>
+ 
+
+<div class="btn-group">
+  <button class="btn btn-large btn-primary" data-toggle="dropdown">Benjamin Kaufman</button>
+  <button class="btn dropdown-toggle btn-large btn-primary" data-toggle="dropdown">
+    <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu">
+    <li><a href="http://www.linkedin.com/pub/benjamin-kaufman/65/541/7b4" target="_blank">Benjamin Kaufman</a></li>
+    <li><a href="#">Web Designer and Color Wheel Spinner</a></li>
+    <li><a href="mailto:blkaufma@uvm.edu">blkaufma@uvm.edu</a></li>
+
+  </ul>
+</div>
+
+<div class="btn-group">
+  <button class="btn btn-large btn-primary" data-toggle="dropdown">Will smith</button>
+  <button class="btn dropdown-toggle btn-large btn-primary" data-toggle="dropdown">
+    <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu">
+    <li><a href="http://www.linkedin.com/profile/view?id=79893085&authType=NAME_SEARCH&authToken=RE9Z&locale=en_US&srchid=2312507441386020217454&srchindex=1&srchtotal=4&trk=vsrp_people_res_name&trkInfo=VSRPsearchId%3A2312507441386020217454%2CVSRPtargetId%3A79893085%2CVSRPcmpt%3Aprimary" target="_blank">Will Smith</a></li>
+    <li><a href="#">Web Guru and Head Honcho</a></li>
+    <li><a href="mailto:g.williamsmithiv@gmail.com">g.williamsmithiv@gmail.com</a></li>
+
+  </ul>
+</div>
+</div><!--closes container -->
