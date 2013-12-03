ef7267c70e6de527fc261907edcb16af3fa9abea
Benjamin Kaufman <blkaufma@uvm.edu>
2013-12-02 23:17:00 -0500

    Update dbRetrieveMyCards.php

--- dbRetrieveMyCards.php
+++ dbRetrieveMyCards.php
@@ -1,8 +1,14 @@
 <?php
 	require("dbconnect.php");
+	if(empty($_SESSION['user'])){
+		header("Location: home.php");
+		die();
+	}//end if
+	$array = $_SESSION['user'];
+	$username = $array['name'];
 	$pagetop = $_POST['page'] * 15;
 	$pagebottom = $pagetop-15;
-	$query = "SELECT * FROM UserCards ORDER BY name Asc LIMIT $pagebottom, $pagetop";
+	$query = "SELECT * FROM UserCards WHERE UserID='$username' ORDER BY name Asc LIMIT $pagebottom, $pagetop;";
 	$getResults = $db->prepare($query);
 	$getResults->execute();
 	$results = $getResults->fetchAll();
